81a5af4a486f3dc1fa4016b09eb4e8267db0c86a
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2020-10-15 20:56:30 -0300

    Remove workaround for "Cointainer" typo
    Typo was fixed in the webpages

--- generate.py
+++ generate.py
@@ -114,13 +114,6 @@ def get_talk(url):
                     talk.time_end = datetime.time(int(m.group(3)), int(m.group(4)))
 
             if tagline_info:
-                if talk_id == 'zarpale-la-data':
-                    if 'Cointainer' in tagline_info:
-                        log.warning("Fixing typo in zarpale-la-data")
-                        tagline_info = tagline_info.replace('Cointainer', 'Container')
-                    else:
-                        log.warning("Typo in zarpale-la-data was fixed; remove workaround")
-
                 m = re.match('(\d+) de [Oo]ctubre [-–] (Containers? [A-Za-z ]+|Keynote)', tagline_info)
                 if m:
                     talk.day = datetime.date(2020, 10, int(m.group(1)))
